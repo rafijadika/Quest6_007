12157de7471f5a169956d259a69159d0f412f38d
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-11-22 15:44:52 +0700

    membuat exposed drop down menu box

--- app/src/main/java/com/example/pertemuan7/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/pertemuan7/ui/widget/DynamicSelectedField.kt
@@ -1,4 +1,13 @@
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import java.lang.reflect.Modifier
 
 @Composable
@@ -9,5 +18,30 @@ fun DynamicSelectedField(
     onValueChangedEvent: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var expanded
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label)},
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = {expanded = false}) {
+            option.forEach { option: String ->
+                DropdownMenuItem(exp)
+            }
+        }
+    }
 }
