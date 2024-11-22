4041895cc4813ec886c3484b89bb741a5323d1d9
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-11-22 15:51:39 +0700

    membuat mata kuliah

--- app/src/main/java/com/example/pertemuan7/Data/MataKuliah.kt
+++ app/src/main/java/com/example/pertemuan7/Data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.pertemuan7.Data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
 }
